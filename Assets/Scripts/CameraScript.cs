4c0d22962e7115bea3d68b1847173fd2f3046a89
Unknown <kelseyhu2425@gmail.com>
2019-10-15 21:21:22 -0400

    Player + camera basic movement

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Transform lookAtTarget;
+    public float speed = 0f;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = playerTransform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
+
+        transform.position = smoothedPosition;
+
+        transform.LookAt(lookAtTarget);
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody myRB;
+    private CapsuleCollider myCol;
+
+    public LayerMask groundLayers;
+    public float moveSpeed = 0f;
+    public float jumpForce = 0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+        myCol = GetComponent<CapsuleCollider>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        moveDirection *= moveSpeed;
+
+        transform.position += moveDirection * Time.deltaTime;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (isGrounded() && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            Debug.Log("key pressed");
+            myRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool isGrounded()
+    {
+       return Physics.CheckCapsule(myCol.bounds.center,
+            new Vector3(myCol.bounds.center.x, myCol.bounds.min.y, myCol.bounds.center.z), myCol.radius * 0.9f, groundLayers);
+    }
+
+}
